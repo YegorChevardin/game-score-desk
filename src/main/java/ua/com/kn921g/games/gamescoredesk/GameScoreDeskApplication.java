318b63b0cbe5c8bf2765c36f0dc4cc4f666c9e15
YegorChevardin <egor03052004@gmail.com>
2024-03-21 17:57:17 +0100

    init

---
+++ src/main/java/ua/com/kn921g/games/gamescoredesk/GameScoreDeskApplication.java
@@ -0,0 +1,13 @@
+package ua.com.kn921g.games.gamescoredesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameScoreDeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameScoreDeskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/kn921g/games/gamescoredesk/GameScoreDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.kn921g.games.gamescoredesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameScoreDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
